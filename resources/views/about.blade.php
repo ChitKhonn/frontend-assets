b837b1e1851348b83d728b294cbc396b32260707
ckc <chitkhonncho9@gmail.com>
2023-09-23 23:19:16 +0630

    add bootstrap

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,11 +1,14 @@
 @extends('master')
 @section('content')
-    <h1 class="header">
-        I'm about page.
-    </h1>
-    <p class="para">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, quod? Quaerat ad pariatur maxime exercitationem
-        enim aperiam ullam dolorum! Dignissimos.
-    </p>
-    <button class="btn" id="showName">Show Name</button>
+    <div class="m-3">
+        <h1 class="text-primary">
+            I'm index page.
+        </h1>
+        <p class="text-black-50">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, quod? Quaerat ad pariatur maxime exercitationem
+            enim aperiam ullam dolorum! Dignissimos.
+        </p>
+        <button class="btn btn-primary" id="showName">Show My Name</button>
+        <a href="{{route('index')}}">Index Page</a>
+    </div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,14 @@
 @extends('master')
 @section('content')
-    <h1 class="header">
-        I'm index page.
-    </h1>
-    <p class="para">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, quod? Quaerat ad pariatur maxime exercitationem
-        enim aperiam ullam dolorum! Dignissimos.
-    </p>
-    <button class="btn" id="greeting">Say Hello</button>
+    <div class="m-3">
+        <h1 class="text-primary">
+            I'm index page.
+        </h1>
+        <p class="text-black-50">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, quod? Quaerat ad pariatur maxime exercitationem
+            enim aperiam ullam dolorum! Dignissimos.
+        </p>
+        <button class="btn btn-primary" id="greeting">Say Hello</button>
+        <a href="{{route('about')}}">About Page</a>
+    </div>
 @endsection
