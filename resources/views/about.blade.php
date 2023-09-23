a4fd8fcb76c39ba1cfa1f5acef8fcbcae060544c
ckc <chitkhonncho9@gmail.com>
2023-09-23 21:53:10 +0630

    simple say hell and show name view

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+@extends('master')
+@section('content')
+    <h1 class="header">
+        I'm about page.
+    </h1>
+    <p class="para">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, quod? Quaerat ad pariatur maxime exercitationem
+        enim aperiam ullam dolorum! Dignissimos.
+    </p>
+    <button class="btn" id="showName">Show Name</button>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('master')
+@section('content')
+    <h1 class="header">
+        I'm index page.
+    </h1>
+    <p class="para">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, quod? Quaerat ad pariatur maxime exercitationem
+        enim aperiam ullam dolorum! Dignissimos.
+    </p>
+    <button class="btn" id="greeting">Say Hello</button>
+@endsection
