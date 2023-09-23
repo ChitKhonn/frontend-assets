4423253dbb36039c26c0507c15ea292f2c84fe96
ckc <chitkhonncho9@gmail.com>
2023-09-23 23:30:34 +0630

    add animation css

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="m-3">
         <h1 class="text-primary">
-            I'm index page.
+            I'm about page.
         </h1>
         <p class="text-black-50">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, quod? Quaerat ad pariatur maxime exercitationem
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,9 @@
 @extends('master')
 @section('content')
     <div class="m-3">
-        <h1 class="text-primary">
+        <h1 class="text-primary animate__animated animate__bounce">
             I'm index page.
+            <i class="bi bi-house"></i>
         </h1>
         <p class="text-black-50">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, quod? Quaerat ad pariatur maxime exercitationem
